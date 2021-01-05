35e1be36e72b957b929e11b934748dbc59ce90bb
yushicheng <1542874601@qq.com>
2021-01-05 18:03:27 +0800

    temp:临时节点标记 星期二 晚上 6:03:27 一月 5日 2021 创建了 gulpfile.js 修改了 .babelrc.js 修改了 package.json

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+/* eslint-disable no-undef */
+const gulp = require("gulp");
+const path = require("path");
+const watch = require("gulp-watch");
+const babel = require("gulp-babel");
+
+function static_task(){
+  const watch_pattern=path.resolve(__dirname,"./src/**/*.{png,jpg,jpeg,gif,svg}");
+  const watcher=watch(watch_pattern,{ignoreInitial:false});
+  watcher.pipe(gulp.dest("dist"));
+  return watcher;
+}
+
+function bebel_task(){
+  const watch_pattern=path.resolve(__dirname,"./src/**/*.{js,jsx}");
+  const watcher=watch(watch_pattern,{ignoreInitial:false},()=>{
+    gulp
+      .src(watch_pattern)
+      .pipe(babel())
+      .pipe(gulp.dest("dist"));
+  });
+  return watcher;
+}
+
+exports.default=gulp.parallel(static_task,bebel_task);
