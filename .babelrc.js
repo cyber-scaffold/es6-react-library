2b566913f282a09264363ff7d403144b91af8dd9
yushicheng <1542874601@qq.com>
2021-03-25 12:40:45 +0800

    temp:临时节点标记 星期四 下午 12:40:45 三月 25日 2021 修改了 .babelrc.js

--- .babelrc.js
+++ .babelrc.js
@@ -1,27 +1,25 @@
 /* eslint-disable */
-const deepExtend = require("deep-extend");
 const basic_babel_config = require("@cyber-tools/preset-babel-option");
 
 
-module.exports = deepExtend(basic_babel_config, {
-  plugins: [
-    [require.resolve("babel-plugin-css-modules-transform"), {
-      devMode: true,
-      keepImport: true,
-      extensions: [".scss"],
-      generateScopedName: "[name]__[local]___[hash:8]",
-      extractCss: {
-        dir: "./dist/",
-        relativeRoot: "./src/",
-        filename: "[path]/[name].scss"
-      }
-    }],
-    [require.resolve("babel-plugin-module-resolver"),{
-      root: ["./src/"],
-      alias: {
-        "@": "./src/",
-        "@@":"./"
-      }
-    }]
-  ]
-});
+basic_babel_config.plugins.push([require.resolve("babel-plugin-css-modules-transform"), {
+  devMode: true,
+  keepImport: true,
+  extensions: [".scss"],
+  generateScopedName: "[name]__[local]___[hash:8]",
+  extractCss: {
+    dir: "./dist/",
+    relativeRoot: "./src/",
+    filename: "[path]/[name].scss"
+  }
+}]);
+
+basic_babel_config.plugins.push([require.resolve("babel-plugin-module-resolver"), {
+  root: ["./src/"],
+  alias: {
+    "@": "./src/",
+    "@@":"./"
+  }
+}]);
+
+module.exports = basic_babel_config
