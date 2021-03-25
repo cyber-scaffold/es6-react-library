271ae0efedb45f4f3b8b0b61bbb625cb98df0355
yushicheng <1542874601@qq.com>
2021-03-25 14:14:31 +0800

    temp:临时节点标记 星期四 下午 2:14:30 三月 25日 2021 修改了 .babelrc.js 修改了 src/TestDemo1/index.js 删除了 src/TestDemo1/b0cacde2c6646a1a0d5c8ec735606b97.jpeg

--- .babelrc.js
+++ .babelrc.js
@@ -22,4 +22,24 @@ basic_babel_config.plugins.push([require.resolve("babel-plugin-module-resolver")
   }
 }]);
 
+/**
+ * @description 按需加载antd组件库
+ * @link https://www.npmjs.com/package/babel-plugin-import
+ * **/
+basic_babel_config.plugins.push([require.resolve("babel-plugin-import"),{
+  libraryName: "antd",
+  libraryDirectory: "lib",
+  style: true
+},"antd"]);
+
+/**
+ * @description 按需加载antd-mobile组件库
+ * @link https://www.npmjs.com/package/babel-plugin-import
+ * **/
+basic_babel_config.plugins.push([require.resolve("babel-plugin-import"),{
+   libraryName: "antd-mobile",
+   libraryDirectory: "es",
+   style: true
+},"antd-mobile"]);
+
 module.exports = basic_babel_config
--- src/TestDemo1/index.js
+++ src/TestDemo1/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import classnames from "classnames";
 
-import css from "./style.scss";
+import css from "./style.less";
 
 export default function TestDemo1() {
-  return <div className={classnames(css.container)}>wellcome to use cyber-es6-lib</div>
-}
+  return (
+    <div className={classnames(css.container)}>
+      <div>wellcome to use cyber-es6-lib</div>
+    </div>
+)}
