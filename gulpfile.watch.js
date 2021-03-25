9e58943e8350a7e25d68f05c32eb432fab6e6bae
yushicheng <1542874601@qq.com>
2021-03-25 11:47:20 +0800

    temp:临时节点标记 星期四 中午 11:47:20 三月 25日 2021 修改了 gulpfile.watch.js

--- gulpfile.watch.js
+++ gulpfile.watch.js
@@ -1,29 +1,28 @@
 /* eslint-disable no-undef */
-const gulp = require("gulp");
-const path = require("path");
-const watch = require("gulp-watch");
-const babel = require("gulp-babel");
-
+const gulp=require("gulp");
+const path=require("path");
+const watch=require("gulp-watch");
+const babel=require("gulp-babel");
 
 function static_task(){
   const watch_pattern=path.resolve(__dirname,"./src/**/*.{css,png,jpg,jpeg,gif,svg,eot,svg,ttf,woff,woff2,json}");
-  const watcher=watch(watch_pattern,{ignoreInitial:false},()=>{
-    gulp
-      .src(watch_pattern,{sourcemaps:true})
-      .pipe(gulp.dest("dist"))
-  });
-  return watcher;
+  const gulp_source=gulp.src(watch_pattern,{sourcemaps:true})
+  gulp_source
+    .pipe(watch(watch_pattern,{ignoreInitial:false}))
+    .pipe(gulp.dest("dist"))
+  return gulp_source;
 }
 
 function bebel_task(){
-  const watch_pattern=path.resolve(__dirname,"./src/**/*.{js,jsx}");
-  const watcher=watch(watch_pattern,{ignoreInitial:false},()=>{
-    gulp
-      .src(watch_pattern,{sourcemaps:true})
-      .pipe(babel())
-      .pipe(gulp.dest("dist"));
-  });
-  return watcher;
+  const source_pattern=path.resolve(__dirname,"./src/**/*.{js,jsx}");
+  const gulp_source=gulp.src(source_pattern,{sourcemaps:true});
+  gulp_source
+    .pipe(watch(source_pattern,{ignoreInitial:false}))
+    .pipe(babel())
+    .pipe(gulp.dest("dist"))
+  return gulp_source;
 }
 
 exports.default=gulp.parallel(static_task,bebel_task);
+
+console.log("watch task is runing");
