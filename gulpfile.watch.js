edc075e67e38697da5fa645635a3ee8ae705d551
yushicheng <1542874601@qq.com>
2021-08-28 14:46:36 +0800

    temp:临时节点标记 星期六 下午 2:46:36 八月 28日 2021 创建了 src/TestDemo3/index.js 创建了 src/TestDemo3/style.scss 修改了 gulpfile.watch.js 修改了 package.json

--- gulpfile.watch.js
+++ gulpfile.watch.js
@@ -3,9 +3,9 @@ const gulp=require("gulp");
 const path=require("path");
 const watch=require("gulp-watch");
 const babel=require("gulp-babel");
+const is_windows=require("is-windows");
 const postcss = require("gulp-postcss");
 // const postcssModules=require("postcss-modules");
-const pxtoviewport = require("postcss-px-to-viewport");
 const postcss_scss=require("postcss-scss");
 
 function static_task(){
@@ -35,16 +35,7 @@ function postcss_task(){
       // postcssModules({
       //   getJSON:()=>{},
       //   generateScopedName: "[name]__[local]___[hash:8]"
-      // }),
-      pxtoviewport({
-        viewportWidth: 750, // (Number) The width of the viewport.
-        viewportHeight: 1624, // (Number) The height of the viewport.
-        unitPrecision: 3, // (Number) The decimal numbers to allow the REM units to grow to.
-        viewportUnit: 'vw', // (String) Expected units.
-        selectorBlackList: ['.ignore', '.hairlines'], // (Array) The selectors to ignore and leave as px.
-        minPixelValue: 1, // (Number) Set the minimum pixel value to replace.
-        mediaQuery: false, // (Boolean) Allow px to be converted in media queries.
-      })
+      // })
     ],{
       parser:postcss_scss
     }))
@@ -53,9 +44,10 @@ function postcss_task(){
 }
 
 const watcher=gulp.watch(path.resolve(__dirname,"./src/**/*.scss"));
-watcher.on('add', gulp.series(bebel_task,postcss_task));
-watcher.on('change', gulp.series(bebel_task,postcss_task));
-watcher.on('unlink', gulp.series(bebel_task,postcss_task));
+is_windows()?watcher.on("raw",gulp.series(bebel_task,postcss_task)):void(0);
+watcher.on("add",gulp.series(bebel_task,postcss_task));
+watcher.on("change",gulp.series(bebel_task,postcss_task));
+watcher.on("unlink",gulp.series(bebel_task,postcss_task));
 
 exports.default=gulp.series(gulp.parallel(static_task,bebel_task),postcss_task);
 
---
+++ src/TestDemo3/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import classnames from "classnames";
+
+import css from "./style.scss";
+
+export default function TestDemo3() {
+  return (
+    <div className={classnames(css.container)}>
+      <div>wellcome to use cyber-es6-lib</div>
+    </div>
+)}
